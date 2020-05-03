dda1866908f64c251a9d12e4fe18996fc35c5148
KarenNgala <ngalalalu@gmail.com>
2020-05-03 23:04:20 +0300

    Added style to head section of html | Changed header texted and added a background image | New method of generating Akan names displaying 'undefined'

---
+++ README.md
@@ -0,0 +1,27 @@
+Akan Name Generator
+This project features a generator that derives an Akan name corresponding to the day of the week that a user was born.
+
+Description
+Akan names are derived from Ghanian culture. Frequently in Ghana, children are given their first name as a 'day name' which corresponds to the day in the week a child is born, and on the gender of the child. This name generator takes a user's birthdate and gender information, and derives an Akan name.
+
+Built With
+HTML
+CSS
+Bootstrap
+Getting Started
+To get a local copy up and running follow these simple steps:
+
+Clone the repo
+git clone https://github.com/trucktar/akan-name-generator.git
+Navigate to the parent folder
+cd akan-name-generator/
+Open the index.html file in a browser of your choice.
+Usage
+If you can quite recall the day of the week you were born, you could use the Akan Names tables to find a corresponding name. Otherwise, enter your birthdate and gender in the form and click Generate to derive your Akan Name.
+
+Contact
+Nyota Mwangi - @trucktar - nyota.benjamin@gmail.com
+Project Link: https://trucktar.github.io/akan-name-generator/
+
+License
+Distributed under the MIT License. See LICENSE for more information.
--- js/script.js
+++ js/script.js
@@ -1,99 +1,91 @@
 //Akan names in gender arrays with Sunday=[0]
-const femaleNames = [
-    "Akosua",
-    "Adwoa",
-    "Abenaa",
-    "Akua",
-    "Yaa",
-    "Afua",
-    "Ama"
-];
-
-const maleNames = [
-    "Kwasi",
-    "Kwadwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi",
-    "Kwame"
-];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 //get values from form
-var Year = document.forms["myform"]["year"].value;
-var a = document.forms["myform"]["month"].value;
-var mm = a.options[a.selectedIndex].value;
-var DD = document.forms["myform"]["date"].value;
-var b = document.forms["myform"]["gender"];
+var Year = document.forms.myform.year.value;
+var mm = document.forms.myform.month.value;
+var DD = document.forms.myform.date.value;
+var b = document.forms.myform.gender;
 var sex = b.options[b.selectedIndex].value;
 
 //split Year into year-part and century-part
 var cc = parseInt(Year / 100);
 var yy = Year % 100;
 
-//Initialization
-var YY;
-var MM;
-var CC;
-var LY;
-var day
-
-//Calculating day of week
-
 // year code, YY
-YY = [yy + (parseInt(yy / 4))] % 7;
+var YY = [yy + (parseInt(yy / 4))] % 7;
+
 //month code, MM
-if (mm == 'January') {
-    MM = 0;
-} else if (mm == 'February') {
-    MM = 3;
-} else if (mm == 'March') {
-    MM = 3;
-} else if (mm == 'April') {
-    MM = 6;
-} else if (mm == 'May') {
-    MM = 1;
-} else if (mm == 'June') {
-    MM = 4;
-} else if (mm == 'July') {
-    MM = 6;
-} else if (mm == 'August') {
-    MM = 2;
-} else if (mm == 'September') {
-    MM = 5;
-} else if (mm == 'October') {
-    MM = 0;
-} else if (mm == 'November') {
-    MM = 3;
-} else if (mm == 'December') {
-    MM = 5;
-} else {
-    alert("Choose a month")
-}
-//century codes, CC
-if (year >= 1800 && year <= 1899) {
-    CC = 2;
-} else if (year >= 1900 && year <= 1999) {
-    CC = 0;
-} else if (year >= 2000 && year <= 2099) {
-    CC = 6;
-} else {
-    alert("Enter a year between 1900 and 2020")
-}
-//leap year, LY
-if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0) {
-    if (mm == 'January' || mm == 'February') {
-        day = (YY + MM + CC + DD - 1) % 7;
+var MM = function monthCode() {
+    if (mm == '1') {
+        MM = 0;
+    } else if (mm == '2') {
+        MM = 3;
+    } else if (mm == '3') {
+        MM = 3;
+    } else if (mm == '4') {
+        MM = 6;
+    } else if (mm == '5') {
+        MM = 1;
+    } else if (mm == '6') {
+        MM = 4;
+    } else if (mm == '7') {
+        MM = 6;
+    } else if (mm == '8') {
+        MM = 2;
+    } else if (mm == '9') {
+        MM = 5;
+    } else if (mm == '10') {
+        MM = 0;
+    } else if (mm == '11') {
+        MM = 3;
+    } else if (mm == '12') {
+        MM = 5;
     }
-} else {
-    day = (YY + MM + CC + DD) % 7;
+    return MM;
 }
 
+//century codes, CC
+var CC = function centuryCode() {
+    if (year >= 1800 && year <= 1899) {
+        CC = 2;
+    } else if (year >= 1900 && year <= 1999) {
+        CC = 0;
+    } else if (year >= 2000 && year <= 2099) {
+        CC = 6;
+    }
+    return CC;
+}
+
+//leap year, LY
+var LY = function isYearLeap() {
+    if (year % 4 == 0 & year % 100 != 0) {
+        return LY = true;
+    } else if (year % 400 == 0) {
+        return LY = true;
+    } else {
+        return LY = false;
+    }
+}
+
+var day = function dayOfWeek() {
+    //isYearLeap();
+    //centuryCode();
+    //monthCode();
+    if (LY == true & mm == 1 | 2) {
+        day = (YY + MM + CC + DD - 1) % 7;
+    } else {
+        day = (YY + MM + CC + DD) % 7;
+    }
+    return day;
+}
 
 function getAkanName() {
-    if (sex === 'Female')
-        alert("Your Akan name is: " + femaleNames[day]);
-    else {
+    //dayOfWeek();
+    if (sex == 'Female') {
+        alert("Your Akan name is: Ama");
+    } else {
         alert("Your Akan name is: " + maleNames[day]);
     }
 }
