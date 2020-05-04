49370aa24bea4d1d5c655b99c17b5359a8865f62
KarenNgala <ngalalalu@gmail.com>
2020-05-04 10:36:15 +0300

    Changes made to script.js file | Changed functions from statement functions into expessin functions

--- js/script.js
+++ js/script.js
@@ -3,11 +3,16 @@ const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
 //get values from form
-var Year = document.forms.myform.year.value;
-var mm = document.forms.myform.month.value;
-var DD = document.forms.myform.date.value;
-var b = document.forms.myform.gender;
-var sex = b.options[b.selectedIndex].value;
+Year = document.forms.myform.year.value;
+mm = document.forms.myform.month.value;
+DD = document.forms.myform.date.value;
+b = document.forms.myform.gender;
+sex = b.options[b.selectedIndex].value;
+
+var Year = parseInt(Year);
+var mm = parseInt(mm);
+var DD = parseInt(DD);
+var sex = toString(sex);
 
 //split Year into year-part and century-part
 var cc = parseInt(Year / 100);
@@ -17,76 +22,69 @@ var yy = Year % 100;
 var YY = [yy + (parseInt(yy / 4))] % 7;
 
 //month code, MM
-var MM = function monthCode() {
+var MM = function() {
     if (mm == '1') {
-        MM = 0;
+        return MM = 0;
     } else if (mm == '2') {
-        MM = 3;
+        return MM = 3;
     } else if (mm == '3') {
-        MM = 3;
+        return MM = 3;
     } else if (mm == '4') {
-        MM = 6;
+        return MM = 6;
     } else if (mm == '5') {
-        MM = 1;
+        return MM = 1;
     } else if (mm == '6') {
-        MM = 4;
+        return MM = 4;
     } else if (mm == '7') {
-        MM = 6;
+        return MM = 6;
     } else if (mm == '8') {
-        MM = 2;
+        return MM = 2;
     } else if (mm == '9') {
-        MM = 5;
+        return MM = 5;
     } else if (mm == '10') {
-        MM = 0;
+        return MM = 0;
     } else if (mm == '11') {
-        MM = 3;
+        return MM = 3;
     } else if (mm == '12') {
-        MM = 5;
+        return MM = 5;
     }
-    return MM;
 }
 
 //century codes, CC
-var CC = function centuryCode() {
-    if (year >= 1800 && year <= 1899) {
-        CC = 2;
-    } else if (year >= 1900 && year <= 1999) {
-        CC = 0;
-    } else if (year >= 2000 && year <= 2099) {
-        CC = 6;
+var CC = function() {
+    if (Year >= 1800 && Year <= 1899) {
+        return CC = 2;
+    } else if (Year >= 1900 && Year <= 1999) {
+        return CC = 0;
+    } else if (Year >= 2000 && Year <= 2099) {
+        return CC = 6;
     }
-    return CC;
 }
 
 //leap year, LY
-var LY = function isYearLeap() {
-    if (year % 4 == 0 & year % 100 != 0) {
+var LY = function() {
+    if (Year % 4 == 0 & Year % 100 != 0) {
         return LY = true;
-    } else if (year % 400 == 0) {
+    } else if (Year % 400 == 0) {
         return LY = true;
     } else {
         return LY = false;
     }
 }
 
-var day = function dayOfWeek() {
-    //isYearLeap();
-    //centuryCode();
-    //monthCode();
+//Calculating day of the week
+var day = function() {
     if (LY == true & mm == 1 | 2) {
-        day = (YY + MM + CC + DD - 1) % 7;
+        return day = (YY + MM + CC + DD - 1) % 7;
     } else {
-        day = (YY + MM + CC + DD) % 7;
+        return day = (YY + MM + CC + DD) % 7;
     }
-    return day;
 }
 
 function getAkanName() {
-    //dayOfWeek();
-    if (sex == 'Female') {
-        alert("Your Akan name is: Ama");
+    if (sex === "Female") {
+        alert("Your Akan name is: " + femaleNames[day]);
     } else {
         alert("Your Akan name is: " + maleNames[day]);
     }
-    return false;
 }
