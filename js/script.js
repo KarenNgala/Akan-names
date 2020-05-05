e110251f88b69330e0ed97304b5b794ee223c532
KarenNgala <ngalalalu@gmail.com>
2020-05-05 11:50:16 +0300

    Js code still displaying 'undefined' despite several attemts to change code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
-var yy = document.myform.year.value;
-var mm = document.myform.month.value;
-var dd = document.myform.date.value;
-var gender = document.myform.gender.value;
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+var yy = parseInt(document.getElementById("year").value);
+var mm = parseInt(document.getElementById("month").value);
+var dd = parseInt(document.getElementById("date").value);
+var gender = document.form.gender.value;
 
 // months are zero indexed so May is 4, not 5
-function getDay(y, m, d, g) {
-    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var d = new Date(y, --m, d);
-    if (g === 'Female') {
-        return d && femaleNames[d.getDay()];
-    } else {
-        return d && maleNames[d.getDay()];
-    }
-}
+function getDay() {
+    var day = new Date(yy, --mm, dd);
+    var d = day.getDay();
 
-function displayName() {
-    alert("You Akan name is:  " + getDay(yy, mm, dd, gender));
+    var akan = "";
+    if (gender === 'Female') {
+        akan = femaleNames[d];
+    } else {
+        akan = maleNames[d];
+    }
+    alert("Your Akan name is: " + akan);
 }
